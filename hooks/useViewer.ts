32eeb60deacc5d6f905d45b82185d69d838e101c
locnelor <1750354949@qq.com>
2024-05-21 17:17:49 +0800

    init

--- hooks/useViewer.ts
+++ hooks/useViewer.ts
@@ -1,10 +1,9 @@
 import { getQuery } from "@/lib/client"
 import viewer from "@/queries/viewer.gql"
-import { user } from "@prisma/client";
 import React from "react";
 
 export const getViewer = async () => {
-    const { data, error } = await getQuery<{ viewer: user }>(viewer);
+    const { data, error } = await getQuery<{ viewer: any }>(viewer);
     return { data, error }
 }
 
