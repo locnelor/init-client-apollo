c0443146b73867023b64070d52030a5579e51e07
locnelor <1750354949@qq.com>
2024-05-21 17:06:09 +0800

    init

--- hooks/useClientViewer.ts
+++ hooks/useClientViewer.ts
@@ -2,9 +2,8 @@
 
 import { useQuery } from "@apollo/client";
 import viewer from "@/queries/viewer.gql"
-import { user } from "@prisma/client"
 const useClientViewer = () => {
-    const { data, loading, error } = useQuery<{ viewer: user }>(viewer)
+    const { data, loading, error } = useQuery<{ viewer: any }>(viewer)
     return { user: data?.viewer, loading, error }
 }
 export default useClientViewer
